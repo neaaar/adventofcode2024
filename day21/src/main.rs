592a723b767abd5d77eb4466b5d3f9a78d9cfa49
Marco Farioli <99016665+neaaar@users.noreply.github.com>
2024-12-21 16:49:45 +0100

    python only today

---
+++ day21/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs;
+use std::time::Instant;
+
+fn main() {
+    let contents = fs::read_to_string("input.txt").expect("Error while reading file");
+    let now = Instant::now();
+
+    //start parsing
+    for lines in contents.lines() {}
+    let elapsed_parsing = now.elapsed();
+
+    //done with parsing, can start solving part 1
+    let now = Instant::now();
+
+    //println!("{}", part1);
+    let elapsed_part1 = now.elapsed();
+
+    //done with part 1, can start solving part 2
+    let now = Instant::now();
+
+    //println!("{}", part2);
+    let elapsed_part2 = now.elapsed();
+
+    println!("Time for parsing: {:.2?}", elapsed_parsing);
+    println!("Time for part 1: {:.2?}", elapsed_part1);
+    println!("Time for part 2: {:.2?}", elapsed_part2);
+}
+
+fn solve(grid: &[Vec<char>], string: String) -> Vec<String> {}
